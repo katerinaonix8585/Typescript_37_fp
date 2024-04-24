8c28a29777cc51a8333f3d40fb1ce849c14c0034
katerinaonix8585 <katerinaonix8585@gmail.com>
2024-04-24 11:13:28 +0200

    new edit in components

---
+++ src/components/AnimalCard/types.tsx
@@ -0,0 +1,9 @@
+interface Animal {
+    name: string,
+    species: string,
+    image: string 
+  }
+  
+export interface AnimalCardProps {
+       animalData:  Animal
+  }
--- src/components/ProfileCard/ProfileCard.tsx
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -1,14 +1,10 @@
 import "./styles.css";
+import { User } from "./types";
 
-interface UserData {
-  avatar: string;
-  userName: string;
-  profession: string;
-  hobbies: string[];
-}
+
 
 function ProfileCard() {
-  const userData : UserData = {
+  const userData : User = {
     avatar:
       "https://img.freepik.com/premium-vector/men-women-different-poses-sitting-standing-isolated-white-background-cute-flat-style-vector-illustration_71599-9116.jpg?w=740",
     userName: "John Doe",
