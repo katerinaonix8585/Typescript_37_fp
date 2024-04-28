9ba8c4c3d90a4b0551105f83196d50c64d420201
katerinaonix8585 <katerinaonix8585@gmail.com>
2024-04-28 15:30:20 +0200

    add Homework09

---
+++ src/homeworks/Homework09/styles.ts
@@ -0,0 +1,51 @@
+import styled from "@emotion/styled";
+
+export const Homework09Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  align-items: center;
+  gap: 30px;
+  padding: 40px;
+`;
+
+export const InputComponent = styled.input`
+  width: 200px;
+  height: 50px;
+  border: 2px solid blue;
+  padding: 10px;
+  font-size: 24px;
+  border-radius: 4px;
+`;
+
+export const LabelComponent = styled.label`
+  font-size: 20px;
+  font-weight: bold; 
+`;
+
+export const TextComponent = styled.label`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 10px;
+  width: 200px;
+  height: 50px;
+  font-size: 24px;
+  color: green;
+  border:  2px black solid;
+`;
+
+export const ButtonComponent = styled.button`
+   width: 200px;
+   height: 70px;
+   outline: none;
+   border: none;
+   border-radius: 4px;
+   padding: 20px;
+   background-color: bisque;
+   color: black;
+   font-size: 20px;
+   font-weight: bold;
+   cursor: pointer;
+`;
+
